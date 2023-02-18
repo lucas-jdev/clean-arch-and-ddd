62bd6bc2c5481d3229bb6f287b6556eaca4d6aeb
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-18 14:07:33 -0300

    init domain

---
+++ src/code/domain/entities/user/User.java
@@ -0,0 +1,72 @@
+package code.domain.entities.user;
+
+import java.util.UUID;
+
+/**
+ * User
+ */
+public class User {
+
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+        this.id = UUID.randomUUID();
+    }
+
+    public User(String username, String email, String password) {
+        super();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(UUID id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void changePassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void changeEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    public UUID id() {
+        return id;
+    }
+
+    public String username() {
+        return username;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public String password() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                User {
+                    username: %s,
+                    email: %s,
+                    password: %s
+                }
+                """.formatted(
+                    username,
+                    email,
+                    password
+                );
+    }
+
+}
---
+++ src/code/domain/repositories/IUserRepository.java
@@ -0,0 +1,21 @@
+package code.domain.repositories;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import code.domain.entities.user.User;
+
+public interface IUserRepository {
+
+    void save(User user);
+
+    Collection<User> findAll();
+
+    Collection<User> findByEmail(String email);
+
+    Collection<User> findByUsername(String username);
+
+    Optional<User> findById(String id);
+
+    void delete(String id);
+}
