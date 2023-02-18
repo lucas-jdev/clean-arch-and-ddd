141d07d2d783fd93824451a0934f5eabd523e5bd
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-18 14:35:00 -0300

    init bean connection database for container spring

---
+++ src/code/infra/framework/spring/IConnectionBean.java
@@ -0,0 +1,17 @@
+package code.infra.framework.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import code.infra.databases.IConnection;
+import code.infra.databases.jdbc.postgreSQL.PostgreSQLConnection;
+
+@Configuration
+public class IConnectionBean {
+
+    @Bean
+    public IConnection connection() {
+        return new PostgreSQLConnection();
+    }
+
+}
