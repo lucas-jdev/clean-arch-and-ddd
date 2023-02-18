121331511d4b70b818fd59fa0494a8fda6d77926
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-18 14:21:46 -0300

    implementation of user operations

---
+++ src/code/infra/databases/jdbc/postgreSQL/UserDatabase.java
@@ -0,0 +1,135 @@
+package code.infra.databases.jdbc.postgreSQL;
+
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.UUID;
+
+import code.domain.entities.user.User;
+import code.domain.repositories.IUserRepository;
+import code.infra.databases.IConnection;
+
+public class UserDatabase implements IUserRepository {
+
+    private final IConnection connection;
+
+    public UserDatabase(IConnection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void save(User user) {
+        Object[] data = {
+                user.username(),
+                user.email(),
+                user.password()
+        };
+
+        connection.insert("""
+                INSERT INTO users (username, email, password)
+                VALUES (?, ?, ?)
+                """, data);
+
+    }
+
+    @Override
+    public Collection<User> findAll() {
+        ResultSet rs = connection.query("""
+                SELECT * FROM users
+                """);
+        return _mapToUsers(rs);
+    }
+
+    @Override
+    public Collection<User> findByEmail(String email) {
+        final var emailWithJokerCharacter = "%" + email + "%";
+
+        ResultSet rs = connection.query("""
+                SELECT * FROM users WHERE email LIKE ?
+                """, emailWithJokerCharacter);
+
+        Collection<User> users = new HashSet<>();
+        try {
+            users = _mapToUsers(rs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    @Override
+    public Collection<User> findByUsername(String username) {
+        final var usernameWithJokerCharacter = "%" + username + "%";
+
+        ResultSet rs = connection.query("""
+                SELECT * FROM users WHERE username LIKE ?
+                """, usernameWithJokerCharacter);
+
+        Collection<User> users = new HashSet<>();
+        try {
+            users = _mapToUsers(rs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return users;
+
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        ResultSet rs = connection.query("""
+                SELECT * FROM users WHERE id::text = ?
+                """, id);
+
+        User user = null;
+        try {
+            user = _getUser(rs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public void delete(String id) {
+        connection.query("""
+                DELETE FROM users WHERE id::text = ?
+                """, id);
+
+    }
+
+    private User _mapToUser(ResultSet rs) throws Exception {
+        String id = rs.getString("id");
+        String username = rs.getString("username");
+        String email = rs.getString("email");
+        String pass = rs.getString("password");
+        UUID uuid = UUID.fromString(id);
+        return new User(uuid, username, email, pass);
+    }
+
+    private Collection<User> _mapToUsers(ResultSet rs) {
+        Collection<User> users = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                users.add(_mapToUser(rs));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    private User _getUser(ResultSet rs) throws Exception {
+        while (rs.next()) {
+            return _mapToUser(rs);
+        }
+        return null;
+    }
+
+}
---
+++ src/code/infra/databases/memory/UserDatabaseInMemory.java
@@ -0,0 +1,59 @@
+package code.infra.databases.memory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import code.domain.entities.user.User;
+import code.domain.repositories.IUserRepository;
+
+public class UserDatabaseInMemory implements IUserRepository {
+
+    private List<User> users = new ArrayList<>();
+
+    @Override
+    public void save(User user) {
+        users.add(user);
+    }
+
+    @Override
+    public Collection<User> findAll() {
+        return users;
+    }
+
+    @Override
+    public Collection<User> findByEmail(String email) {
+        return users.stream()
+                .filter(user -> user.email().contains(email))
+                .toList();
+    }
+
+    @Override
+    public Collection<User> findByUsername(String username) {
+        return users.stream()
+                .filter(user -> user.username().contains(username))
+                .toList();
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        UUID uuid = UUID.fromString(id);
+        return users.stream()
+                .filter(user -> user.id().equals(uuid))
+                .findFirst();
+    }
+
+    @Override
+    public void delete(String id) {
+        UUID uuid = UUID.fromString(id);
+        Optional<User> userOptional = users.stream()
+                .filter(user -> user.id().equals(uuid))
+                .findFirst();
+
+        userOptional.ifPresent(users::remove);
+        userOptional.orElseThrow();
+    }
+
+}
