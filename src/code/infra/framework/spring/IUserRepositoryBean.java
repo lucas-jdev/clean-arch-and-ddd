780c3b871ec64231a32e61ac68ca643e4264c91f
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-18 14:36:06 -0300

    init bean user repository for container spring

---
+++ src/code/infra/framework/spring/IUserRepositoryBean.java
@@ -0,0 +1,18 @@
+package code.infra.framework.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import code.domain.repositories.IUserRepository;
+import code.infra.databases.IConnection;
+import code.infra.databases.jdbc.postgreSQL.UserDatabase;
+
+@Configuration
+public class IUserRepositoryBean {
+
+    @Bean
+    public IUserRepository userRepository(IConnection connection) {
+        return new UserDatabase(connection);
+    }
+    
+}
