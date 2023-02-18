fe774b2f0016bf8914144215bcad424a5e9ce2b8
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-18 14:13:02 -0300

    use case of find user by id

---
+++ src/code/application/use_cases/user/read/user/FindUserById.java
@@ -0,0 +1,21 @@
+package code.application.use_cases.user.read.user;
+
+import java.util.Optional;
+
+import code.application.dtos.user.OutFindUserById;
+import code.domain.entities.user.User;
+import code.domain.repositories.IUserRepository;
+
+public record FindUserById (IUserRepository userRepository) {
+
+    public OutFindUserById execute(String id) {
+        Optional<User> userFound = userRepository.findById(id);
+        return userFound.map(user -> new OutFindUserById(
+                user.id().toString(),
+                user.username(),
+                user.email(),
+                user.password()
+        )).orElse(null);
+    }
+
+}
