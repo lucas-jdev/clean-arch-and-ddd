3fda8d4846d0d6502888e803d5d96749da5a5eff
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-19 16:26:11 -0300

    add field status out find user by username

--- src/code/application/dtos/user/OutFindUserByUsername.java
+++ src/code/application/dtos/user/OutFindUserByUsername.java
@@ -3,7 +3,8 @@ package code.application.dtos.user;
 public record OutFindUserByUsername(
     String id, 
     String username, 
-    String email, 
+    String email,
+    String status,
     String password
 ) {
     
