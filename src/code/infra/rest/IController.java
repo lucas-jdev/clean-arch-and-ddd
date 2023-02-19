5823a32b0108fd599427057157f2a5763326bf3e
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-19 16:39:20 -0300

    modify interface controller

--- src/code/infra/rest/IController.java
+++ src/code/infra/rest/IController.java
@@ -2,12 +2,8 @@ package code.infra.rest;
 
 public interface IController<I,O> {
     
-    O findById(String id);
-
     O save(I entity);
 
     O update(I entity);
-
-    void delete(String id);
 
 }
