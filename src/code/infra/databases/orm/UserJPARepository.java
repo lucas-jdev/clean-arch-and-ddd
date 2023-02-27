ce8045bf6f92f330380e7454f17b75dfd62fda21
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-27 11:27:49 -0300

    user jpa repository methods

--- src/code/infra/databases/orm/UserJPARepository.java
+++ src/code/infra/databases/orm/UserJPARepository.java
@@ -1,9 +1,13 @@
 package code.infra.databases.orm;
 
+import java.util.Collection;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserJPARepository extends JpaRepository<UserEntity, UUID> {
+public interface UserJPARepository extends JpaRepository<UserSchema, UUID> {
+
+    Collection<UserSchema> findByEmail(String email);
+    Collection<UserSchema> findByUsername(String email);
 
 }
