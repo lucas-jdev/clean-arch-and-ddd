db6837095650bc017ad4b7f6563bd1b2b18d5d43
Lucas Jdev <lucas.jdev1@gmail.com>
2023-02-27 11:30:47 -0300

    fundamentals props spring runner

--- src/code/infra/framework/spring/SpringRunner.java
+++ src/code/infra/framework/spring/SpringRunner.java
@@ -1,18 +1,26 @@
 package code.infra.framework.spring;
 
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 
+import code.infra.env.EnvironmentVariables;
 import code.infra.framework.IFramework;
 
 public class SpringRunner implements IFramework{
-    
+  
+    private static final Map<String, Object> properties = new HashMap<>();
+
+    public SpringRunner() {
+      properties.put("server.port", EnvironmentVariables.get("SPRING_PORT"));
+      properties.put("server.servlet.context-path", EnvironmentVariables.get("SPRING_CONTEXT"));
+    }
+
     @Override
     public void runner(Class<?> classMain,String... args) {
         SpringApplication app = new SpringApplication(classMain);
-        app.setDefaultProperties(Collections
-          .singletonMap("server.port", "8083"));
+        app.setDefaultProperties(properties);
         app.run(args);
     }
 }
